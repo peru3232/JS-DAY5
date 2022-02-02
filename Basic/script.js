4b48b1b9b99c810897af3b4abc472fd58bf7f646
peru3232 <prw@qmail.com.de>
2022-02-02 13:10:05 +0100

    ...

--- Basic/script.js
+++ Basic/script.js
@@ -31,9 +31,9 @@ class car extends basic{
     }
 
     output() {
-        return `<div class="col"><div class="card" style="width: 18rem;">
+        return `<div class="col py-3"><div class="card" style="width: 18rem;">
         <div class="card-header">Car Properties</div>
-        <img src="${this.img}" alt="">
+        <img src="${this.img}" height = "150" alt="">
         <ul class="list-group list-group-flush">
           <li class="list-group-item">${this.name}</li>
           <li class="list-group-item">${this.brand}</li>
@@ -48,14 +48,8 @@ class car extends basic{
 
 }
 
-const result = document.getElementById("result");
+// const result = document.getElementById("result");
 
-const Mazda = new car("Heinrich323", "Mazda", "Mazda 3 (Bj 2008 )", "hatchback", "white", "1111kg", "14 Years", "https://cdn.pixabay.com/photo/2018/03/08/19/06/auto-3209511_1280.jpg");
-const Mazda2 = new car("Gundula0815", "Mazda", "Mazda 3 (Bj 2018 )", "not hatchback", "black", "1112kg", "4 Years", "https://cdn.pixabay.com/photo/2016/05/14/02/18/camera-1391325_1280.jpg");
-const Mazda3 = new car("Kunigunde201", "Mazda", "Mazda 3 (Bj 2009 )", "full hatchback", "grey", "1110kg", "13 Years", "https://cdn.pixabay.com/photo/2016/04/29/08/52/car-1360471_1280.jpg");
-result.innerHTML = Mazda.output();
-result.innerHTML += Mazda2.output();
-result.innerHTML += Mazda3.output();
 
 class animal extends basic{
       
